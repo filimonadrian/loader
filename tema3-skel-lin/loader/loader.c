535067ef6adcb7ea0f716f843b3bcad0be8aa0a9
filimonadrian <adrian.filimon@hotmail.com>
2021-04-26 20:15:03 +0300

    first 5 tests passed. infinite loop

--- tema3-skel-lin/loader/loader.c
+++ tema3-skel-lin/loader/loader.c
@@ -18,19 +18,24 @@
 
 static so_exec_t *exec;
 int fd = 0;
-struct sigaction old_handler;
-struct sigaction new_handler;
+
+void find_segment_err()
+{
+	
+}
 
 static void sig_handler(int signum, siginfo_t *info, void *context)
 {
+	so_seg_t *segment = NULL;
+	void *ret = NULL;
 	int page_size = getpagesize();
 	int i = 0;
-	so_seg_t *segment;
 	int *valid_pages;
 	int start_page = 0, end_page = 0;
 	int high_offset = 0, low_offset = 0;
-	void *ret = NULL;
+	int page_index = 0;
 
+	// return;
 	for (i = 0; i < exec->segments_no; i++) {
 		segment = &exec->segments[i];
 		valid_pages = (int *)(segment->data);
@@ -40,10 +45,12 @@ static void sig_handler(int signum, siginfo_t *info, void *context)
 		end_page = segment->vaddr + segment->mem_size;
 
 		/* if the current address is in this segment */
-		if ((int)info->si_addr >= start_page && (int)info->si_addr < end_page) {
+		if ((int)info->si_addr >= start_page &&
+			(int)info->si_addr < end_page) {
 
+			// fprintf(stderr, "EXECUTIA %d\n", i);
 			/* calculate page index */
-			int page_index = (int)(info->si_addr - start_page) / page_size;
+			page_index = (int)(info->si_addr - start_page) / page_size;
 
 			/* if the page was already accessed */
 			if (valid_pages[page_index] != 0) {
@@ -56,7 +63,7 @@ static void sig_handler(int signum, siginfo_t *info, void *context)
 
 			low_offset = segment->offset + high_offset;
 
-			if (high_offset > segment->file_size) {
+			if (high_offset >= segment->file_size) {
 				ret = mmap((void *)(start_page + high_offset), page_size,
 					PROT_WRITE,
 					MAP_PRIVATE | MAP_ANONYMOUS,
@@ -101,65 +108,65 @@ static void sig_handler(int signum, siginfo_t *info, void *context)
 			}
 		}
 	}
-	old_handler.sa_sigaction(signum, info, context);
 }
 
 
 int so_init_loader(void)
 {
-	/* TODO: initialize on-demand loader */
-
-	// struct sigaction signals;
+	struct sigaction signals;
 	int ret = 0;
-	memset(&new_handler, 0, sizeof(struct sigaction));
-	sigemptyset(&new_handler.sa_mask);
+	/* initialize on-demand loader */
 
-	new_handler.sa_flags = SA_RESETHAND;
-	new_handler.sa_handler = (void *)sig_handler;
+	memset(&signals, 0, sizeof(struct sigaction));
+	sigemptyset(&signals.sa_mask);
+	sigaddset(&signals.sa_mask, SIGSEGV);
 
-	ret = sigaction(SIGSEGV, &new_handler, &old_handler);
-	if (ret < 0)
-		fprintf(stderr, "Sigaction\n");
+	signals.sa_flags = SA_SIGINFO;
+	signals.sa_sigaction = sig_handler;
 
-	return -1;
+	ret = sigaction(SIGSEGV, &signals, NULL);
+
+	return ret;
 }
 
 int so_execute(char *path, char *argv[])
 {
 	int i = 0, j = 0;
 	int page_size = 0;
+
 	exec = so_parse_exec(path);
+	page_size = getpagesize();
+
 	if (!exec)
 		return -1;
 
 	fd = open(path, O_RDONLY, 0644);
 	if (fd < 0) {
-		fprintf(stderr, "Oppening file");
 		return -1;
 	}
 
-	page_size = getpagesize();
-
+	// fprintf(stderr, "Segments no: %d\n", exec->segments_no);
 	/* for every segment */
 	for (i = 0; i < exec->segments_no; i++) {
+		// fprintf(stderr, "EXECUTIA %d\n", i);
 		so_seg_t *segment = &exec->segments[i];
 		int nr_pages = 0;
-		/* array for all pages to check if there are already mapped */
-		int *valid_pages;
+		/* array for all pages to check if they are already mapped */
+		// int *valid_pages;
 
+		nr_pages = segment->mem_size / page_size + 1;
+
+		segment->data = calloc (0, nr_pages * sizeof(int));
 		if (segment->data == NULL)
 			return -1;
 
-		nr_pages = segment->mem_size / page_size + 1;
-
-		valid_pages = malloc (nr_pages * sizeof(int));
-		nr_pages = segment->mem_size / page_size + 1;
-
-		for (j = 0; j < nr_pages; j++) {
-			((int*)segment->data)[j] = 0;
-		}
+		// for (j = 0; j < nr_pages; j++) {
+		// 	((int*)segment->data)[j] = 0;
+		// }
 	}
+
 	so_start_exec(exec, argv);
 
-	return -1;
+	// return -1;
+	return 0;
 }
